4f810fd73f89e7bd3652d5b4cda3b426503006c9
Gatij-coder <116671286+Gatij-coder2911@users.noreply.github.com>
2023-04-19 22:58:12 +0530

    Update setup.py

--- Source Code/Project Setup/setup.py
+++ Source Code/Project Setup/setup.py
@@ -4,7 +4,5 @@ import subprocess
 
 subprocess.call("Setup_Files\Module_Installer.bat")
 
-subprocess.call("Setup_Files\Table_Creator.bat")
-
 print("All things are ready now you can start your program!!☺")
 
