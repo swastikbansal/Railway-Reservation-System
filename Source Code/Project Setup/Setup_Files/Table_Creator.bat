37c505db5c1042830a22b4cdd3101041eafb908e
Gatij-coder <116671286+Gatij-coder2911@users.noreply.github.com>
2023-04-19 22:56:37 +0530

    Delete Table_Creator.bat

--- Source Code/Project Setup/Setup_Files/Table_Creator.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo --------Creating Tables----------
-
-set /p password= Enter your Password (mysql login) : 
-
-mysql -uroot -p%password% < "Setup_Files\Create tables\Script\Local Machine.sql"
-echo Your Tables have been Created Successfully!!
-PAUSE
